2064444a325b85a1a96b30c2b172e6847d41be19
henry <henry@orangegrin.com>
2023-06-04 04:02:52 +0800

    append CounterV3 test

--- test/Proxy.t.sol
+++ test/Proxy.t.sol
@@ -6,16 +6,25 @@ import "../src/CounterV1.sol";
 import '../src/CounterV2.sol';
 import "../src/Deployer.sol";
 import "../src/IProxy.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
 
 using { create, appendArg } for bytes;
-
+interface CounterV3 {
+  function setValue(uint256) external;
+  function getValue() external returns (uint256);
+}
 contract ProxyTest is Test {
     address logic1;
     address logic2;
+    address logic3;
 
     function setUp() public {
         logic1 = address(new CounterV1());
         logic2 = address(new CounterV2());
+        logic3 = HuffDeployer.deploy("../huff/CounterV3");
+        
+
     }
 
     function testProxyBasics() public {
@@ -26,8 +35,9 @@ contract ProxyTest is Test {
         assertGt(_after, _before);
         console.log(CounterV1(proxy).version());
         IProxy(proxy).destroy();        
-        proxy = deployProxy(vm, logic2, address(this));
-        console.log(CounterV1(proxy).version());
+        proxy = deployProxy(vm, logic3, address(this));
+        CounterV3(proxy).setValue(1);
+        console.log(CounterV3(proxy).getValue());
 
     }
 
